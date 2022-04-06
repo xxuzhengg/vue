975eec9eb27e15c915e9819b3bad78ecaeb086cc
xuzheng <1580181628@qq.com>
2022-04-06 18:28:27 +0800

    vue

--- vue_advanced/05_src_插件/main.js
+++ vue_advanced/05_src_插件/main.js
@@ -8,9 +8,9 @@ import plugins from './plugins'
 Vue.config.productionTip = false
 
 //应用（使用）插件
-Vue.use(plugins,1,2,3)
+Vue.use(plugins, 1, 2, 3)
 //创建vm
 new Vue({
-	el:'#app',
-	render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
