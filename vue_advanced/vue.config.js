175316684278ac68c5f5c4b5e67b6e50ab491926
xuzheng <1580181628@qq.com>
2022-04-15 11:06:08 +0800

    vue

--- vue_advanced/vue.config.js
+++ vue_advanced/vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    // devServer: {
+    //     proxy: 'http://localhost:9090'
+    // },
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:9090',
+                pathRewrite: {'^/api': ''},
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    },
 })
