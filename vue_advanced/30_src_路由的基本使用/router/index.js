35dd5696b451fb2e55b11f98c0599d082750b935
xuzheng <1580181628@qq.com>
2022-05-01 15:38:58 +0800

    vue

--- vue_advanced/30_src_路由的基本使用/router/index.js
+++ vue_advanced/30_src_路由的基本使用/router/index.js
@@ -1,19 +1,19 @@
 // 该文件专门用于创建整个应用的路由器
 import VueRouter from 'vue-router'
 //引入组件
-import About from '../components/About'
-import Home from '../components/Home'
+import AppAbout from '../pages/AppAbout'
+import AppHome from '../pages/AppHome'
 
 //创建并暴露一个路由器
 export default new VueRouter({
-	routes:[
-		{
-			path:'/about',
-			component:About
-		},
-		{
-			path:'/home',
-			component:Home
-		}
-	]
+    routes: [
+        {
+            path: '/about',
+            component: AppAbout
+        },
+        {
+            path: '/home',
+            component: AppHome
+        }
+    ]
 })
