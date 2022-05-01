f8036dedc4428ec22b41a86d9cdbfbcf0cdaa519
xuzheng <1580181628@qq.com>
2022-05-01 18:20:16 +0800

    vue

--- vue_advanced/32_src_路由的query参数/router/index.js
+++ vue_advanced/32_src_路由的query参数/router/index.js
@@ -1,38 +1,38 @@
 // 该文件专门用于创建整个应用的路由器
 import VueRouter from 'vue-router'
 //引入组件
-import About from '../pages/About'
-import Home from '../pages/Home'
-import News from '../pages/News'
-import Message from '../pages/Message'
-import Detail from '../pages/Detail'
+import AppAbout from '../pages/AppAbout'
+import AppHome from '../pages/AppHome'
+import AppNews from '../pages/AppNews'
+import AppMessage from '../pages/AppMessage'
+import AppDetail from '../pages/AppDetail'
 
 //创建并暴露一个路由器
 export default new VueRouter({
-	routes:[
-		{
-			path:'/about',
-			component:About
-		},
-		{
-			path:'/home',
-			component:Home,
-			children:[
-				{
-					path:'news',
-					component:News,
-				},
-				{
-					path:'message',
-					component:Message,
-					children:[
-						{
-							path:'detail',
-							component:Detail,
-						}
-					]
-				}
-			]
-		}
-	]
+    routes: [
+        {
+            path: '/about',
+            component: AppAbout
+        },
+        {
+            path: '/home',
+            component: AppHome,
+            children: [
+                {
+                    path: 'news',
+                    component: AppNews,
+                },
+                {
+                    path: 'message',
+                    component: AppMessage,
+                    children: [
+                        {
+                            path: 'detail',
+                            component: AppDetail,
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 })
