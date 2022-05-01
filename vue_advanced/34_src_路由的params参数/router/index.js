f75fe459597a0237502732d6b72cef57e9c0aa10
xuzheng <1580181628@qq.com>
2022-05-01 22:00:27 +0800

    vue

--- vue_advanced/34_src_路由的params参数/router/index.js
+++ vue_advanced/34_src_路由的params参数/router/index.js
@@ -1,40 +1,40 @@
 // 该文件专门用于创建整个应用的路由器
 import VueRouter from 'vue-router'
 //引入组件
-import About from '../pages/About'
-import Home from '../pages/Home'
-import News from '../pages/News'
-import Message from '../pages/Message'
-import Detail from '../pages/Detail'
+import AppAbout from '../pages/AppAbout'
+import AppHome from '../pages/AppHome'
+import AppNews from '../pages/AppNews'
+import AppMessage from '../pages/AppMessage'
+import AppDetail from '../pages/AppDetail'
 
 //创建并暴露一个路由器
 export default new VueRouter({
-	routes:[
-		{
-			name:'guanyu',
-			path:'/about',
-			component:About
-		},
-		{
-			path:'/home',
-			component:Home,
-			children:[
-				{
-					path:'news',
-					component:News,
-				},
-				{
-					path:'message',
-					component:Message,
-					children:[
-						{
-							name:'xiangqing',
-							path:'detail/:id/:title',
-							component:Detail,
-						}
-					]
-				}
-			]
-		}
-	]
+    routes: [
+        {
+            name: 'guanyu',
+            path: '/about',
+            component: AppAbout
+        },
+        {
+            path: '/home',
+            component: AppHome,
+            children: [
+                {
+                    path: 'news',
+                    component: AppNews,
+                },
+                {
+                    path: 'message',
+                    component: AppMessage,
+                    children: [
+                        {
+                            name: 'xiangqing',
+                            path: 'detail/:id/:title',
+                            component: AppDetail,
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 })
