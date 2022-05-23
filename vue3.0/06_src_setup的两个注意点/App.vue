dd9ec84ddf44ce610c92baa7de5a621e12d085a5
xuzheng <1580181628@qq.com>
2022-05-23 23:56:45 +0800

    vue3.0

---
+++ vue3.0/06_src_setup的两个注意点/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <AppDemo @hello="showHelloMsg" msg="你好啊" school="尚硅谷">
+    <template v-slot:qwe>
+      <span>尚硅谷</span>
+    </template>
+    <template v-slot:asd>
+      <span>尚硅谷</span>
+    </template>
+  </AppDemo>
+</template>
+
+<script>
+import AppDemo from './components/AppDemo'
+
+export default {
+  name: 'App',
+  components: {AppDemo},
+  setup() {
+    function showHelloMsg(value) {
+      alert(`你好啊，你触发了hello事件，我收到的参数是:${value}！`)
+    }
+
+    return {
+      showHelloMsg
+    }
+  }
+}
+</script>
+
